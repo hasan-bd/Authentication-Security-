4b213e0338474325b6e55683604ef3eca23d9e44
hasan-bd <kamrulhasan570@gmail.com>
2021-06-26 01:56:07 +0600

    Enviroment variable to keep secret all api key and secret file. [lv-3]

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 var encrypt = require('mongoose-encryption');
+console.log(process.env.SECRET);
 
 const app = express();
 
@@ -20,8 +22,7 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-const secret ="This is our little secret. ";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 
