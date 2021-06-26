aeb894d54eb07d94b4b2350b18376f41438a91ad
hasan-bd <kamrulhasan570@gmail.com>
2021-06-26 21:18:37 +0600

    Registrion useing passport package.

--- app.js
+++ app.js
@@ -4,26 +4,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-
+// Step 1 for install cookie and passport indall below 3 npm package
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
+ // Step 2 start here
+app.use(session({
+  secret: 'This is our little secret',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+// Step 2 close
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
-
+mongoose.set('useCreateIndex', true); //step 5//
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
 
-
+userSchema.plugin(passportLocalMongoose);  //Step 3//
 
 const User = mongoose.model('User', userSchema)
+// Step 4 started
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// Step 4 close.
+
+
 //TODO and Write your Code
 app.get('/',function(req,res){
   res.render('home')
@@ -36,13 +58,31 @@ app.get('/login',function(req,res){
 app.get('/register',function(req,res){
   res.render('register')
 })
+// step 7 started
+app.get('/secrets',function(req,res){
+  if(req.isAuthenticated()){
+    res.render('secrets')
+  }else{
+    res.redirect('login')
+  }
+})
+// step 7 colsed
 
-
+// Step 6 for registration purpose started
 app.post('/register',function(req,res){
-
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+  if (err) {
+    res.redirect('/register');
+  }else{
+    // passport come form passport npm package
+    passport.authenticate("local")(req,res,function(){
+      res.redirect('/secrets');
+    })
+  }
+  })
 
 })
-
+// Step 6 close
 
 app.post('/login',function(req,res){
 
