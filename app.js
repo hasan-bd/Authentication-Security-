36dca676315bb3f8323ee480fc700ea5e65d571d
hasan-bd <kamrulhasan570@gmail.com>
2021-06-26 19:29:38 +0600

    Insall passport and cookies some package and remove registration & login

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 
 const app = express();
@@ -22,8 +20,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
-//  Here process.env.SECRET come from .env file that does not upload github for security purpose
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 
 
@@ -43,42 +39,13 @@ app.get('/register',function(req,res){
 
 
 app.post('/register',function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    })
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render('secrets')
-      }
-    })
-});
-
 
 
 })
 
 
 app.post('/login',function(req,res){
-  const username = req.body.username
-  const password = req.body.password
-  User.findOne({email: username},function(err, foundUser){
-    if(err){
-      console.log("err");
-    }else{
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result==true){
-              res.render('secrets')
-            }
-          });
-      }
-    }
-  })
+
 })
 
 
