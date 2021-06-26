85b25509cd6d65af0996ea5d7b02f973cd8fae58
hasan-bd <kamrulhasan570@gmail.com>
2021-06-26 21:29:15 +0600

    Login system using passrot npm package.

--- app.js
+++ app.js
@@ -85,7 +85,19 @@ app.post('/register',function(req,res){
 // Step 6 close
 
 app.post('/login',function(req,res){
-
+const user = new User ({
+  username: req.body.username,
+  password: req.body.password
+})
+req.login(user, function(err) {
+  if (err) {
+     console.log(err);
+   }else{
+     passport.authenticate("local")(req,res,function(){
+       res.redirect('/secrets');
+   })
+ }
+})
 })
 
 
