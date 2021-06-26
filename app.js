437dfe1ca7baf70a1b5048b302ac9cce5077dad2
hasan-bd <kamrulhasan570@gmail.com>
2021-06-26 21:34:16 +0600

    Logout System using passport npm package.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
- // Step 2 start here
+// Step 2 start here
 app.use(session({
   secret: 'This is our little secret',
   resave: false,
@@ -28,14 +28,17 @@ app.use(passport.initialize());
 app.use(passport.session());
 // Step 2 close
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.set('useCreateIndex', true); //step 5//
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
 
-userSchema.plugin(passportLocalMongoose);  //Step 3//
+userSchema.plugin(passportLocalMongoose); //Step 3//
 
 const User = mongoose.model('User', userSchema)
 // Step 4 started
@@ -47,59 +50,72 @@ passport.deserializeUser(User.deserializeUser());
 
 
 //TODO and Write your Code
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
   res.render('home')
 })
 
-app.get('/login',function(req,res){
+app.get('/login', function(req, res) {
   res.render('login')
 })
 
-app.get('/register',function(req,res){
+app.get('/register', function(req, res) {
   res.render('register')
 })
+
+// Step 9 logout system code start here
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+// Logout code close
+
+
 // step 7 started
-app.get('/secrets',function(req,res){
-  if(req.isAuthenticated()){
+app.get('/secrets', function(req, res) {
+  if (req.isAuthenticated()) {
     res.render('secrets')
-  }else{
+  } else {
     res.redirect('login')
   }
 })
 // step 7 colsed
 
 // Step 6 for registration purpose started
-app.post('/register',function(req,res){
-  User.register({username: req.body.username}, req.body.password, function(err, user) {
-  if (err) {
-    res.redirect('/register');
-  }else{
-    // passport come form passport npm package
-    passport.authenticate("local")(req,res,function(){
-      res.redirect('/secrets');
-    })
-  }
+app.post('/register', function(req, res) {
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      res.redirect('/register');
+    } else {
+      // passport come form passport npm package
+      passport.authenticate("local")(req, res, function() {
+        res.redirect('/secrets');
+      })
+    }
   })
 
 })
 // Step 6 close
 
-app.post('/login',function(req,res){
-const user = new User ({
-  username: req.body.username,
-  password: req.body.password
-})
-req.login(user, function(err) {
-  if (err) {
-     console.log(err);
-   }else{
-     passport.authenticate("local")(req,res,function(){
-       res.redirect('/secrets');
-   })
- }
-})
-})
 
+// Step 8 login system started here
+app.post('/login', function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect('/secrets');
+      })
+    }
+  })
+})
+// step 8 login systerm close.
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
